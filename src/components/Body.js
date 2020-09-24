b1c88ebb990d5224b20fc3ff78e17372b1ac84a4
Airgrinder <51jework@mail.com>
2020-09-25 00:36:11 +0300

    2509a

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,35 +5,64 @@ export default class Body extends Component {
         super(props)
 
         this.state = {
-
-        }
-        generateMail() {
-
+            basicData: "lion",
+            modifierData: 1,
+            mailServiceData: "gmail.com",
+            generatedResult: "",
+            data: []
         }
     }
+
+    generateCredential = () => {
+        this.setState(state => ({
+            modifierData: this.state.modifierData + 1,
+            data: [...state.data, {
+                email: this.state.basicData + this.state.modifierData + "@" + this.state.mailServiceData,
+                pass: this.makeID(),
+            }]
+        }))
+    }
+
+    makeID = () => {
+        const makePathID = (length) => {
+            let text = "";
+            const char_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            for (let i = 0; i < length; i++) {
+                text += char_list.charAt(Math.floor(Math.random() * char_list.length));
+            }
+            return text;
+        }
+        return makePathID(12)
+    }
+
     render() {
+        const { data } = this.state
         return (
             <div className="App">
                 <div className="body">
                     <div className="body-input">
-                        <div className="body-input__basic"><input type="text" placeholder='Enter mailbox name. Example: "Alex"' /></div>
+                        <div className="body-input__basic"><input onChange={() => { }} type="text" placeholder='Enter mailbox name. Example: "Alex"' /></div>
                         <div className="body-input__modifier"><input type="text" placeholder='Enter the modifier. Example: "1"' /></div>
                         <div className="body-input__@"><h1>@</h1></div>
                         <div className="body-input__mail-service"><input type="text" placeholder='Enter mail service name. Example: "gmail.com"' /></div>
-                        <div className="body-input__button"><button type="submit">generate</button></div>
+                        <div className="body-input__button"><button onClick={this.generateCredential} type="submit">generate</button></div>
                     </div>
                     <div className="body-settings">
                         <div className="body-settings__randomPassword-title">generate random password</div>
                         <div className="body-settings__randomPassword-checkbox"><input type="checkbox" /></div>
                     </div>
                     <div className="body-list">
-                        <div className="body-list__headline">Generate list</div>
+                        <div className="body-list__headline">Generated list</div>
                         <div className="body-list__generate-list">
-
+                            {
+                                data.length ? data.reverse().map((obj) => {
+                                    return <p>{obj.email} - {obj.pass} <button onClick={() => document.execCommand("copy")}>copy</button></p>
+                                }) : <p>error, empty input</p>
+                            }
                         </div>
                     </div>
                 </div>
-            </div>
+            </div >
         )
     }
 }
