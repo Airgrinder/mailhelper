aa5de0a4de83c7028cd6dcc1c99acd534a70bbc9
Airgrinder <51jework@mail.com>
2020-09-28 01:24:59 +0300

    inputs2809

--- src/components/Body.js
+++ src/components/Body.js
@@ -13,17 +13,35 @@ export default class Body extends Component {
         }
     }
 
+    basicChange = (event) => {
+        this.setState({
+            basicData: event.target.value
+        })
+    }
+
+    modifierChange = (event) => {
+        this.setState({
+            modifierData: parseInt(event.target.value, 10)
+        })
+    }
+
+    mailServiceData = (event) => {
+        this.setState({
+            mailServiceData: event.target.value
+        })
+    }
+    
     generateCredential = () => {
         this.setState(state => ({
             modifierData: this.state.modifierData + 1,
             data: [...state.data, {
                 email: this.state.basicData + this.state.modifierData + "@" + this.state.mailServiceData,
-                pass: this.makeID(),
+                pass: this.makePass(),
             }]
         }))
     }
 
-    makeID = () => {
+    makePass = () => {
         const makePathID = (length) => {
             let text = "";
             const char_list = "ABCDEFGHIJKLMNOQRSTPUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -41,10 +59,10 @@ export default class Body extends Component {
             <div className="App">
                 <div className="body">
                     <div className="body-input">
-                        <div className="body-input__basic"><input onChange={() => { }} type="text" placeholder='Enter mailbox name. Example: "Alex"' /></div>
-                        <div className="body-input__modifier"><input type="text" placeholder='Enter the modifier. Example: "1"' /></div>
+                        <div className="body-input__basic"><input onChange={this.basicChange} type="text" placeholder='Enter mailbox name. Example: "Alex"' /></div>
+                        <div className="body-input__modifier"><input onChange={this.modifierChange} type="text" placeholder='Enter the number modifier. Example: "1"' /></div>
                         <div className="body-input__@"><h1>@</h1></div>
-                        <div className="body-input__mail-service"><input type="text" placeholder='Enter mail service name. Example: "gmail.com"' /></div>
+                        <div className="body-input__mail-service"><input onChange={this.mailServiceData} type="text" placeholder='Enter mail service name. Example: "gmail.com"' /></div>
                         <div className="body-input__button"><button onClick={this.generateCredential} type="submit">generate</button></div>
                     </div>
                     <div className="body-settings">
@@ -57,7 +75,7 @@ export default class Body extends Component {
                             {
                                 data.length ? data.reverse().map((obj) => {
                                     return <p>{obj.email} - {obj.pass} <button onClick={() => document.execCommand("copy")}>copy</button></p>
-                                }) : <p>error, empty input</p>
+                                }) : <p>empty</p>
                             }
                         </div>
                     </div>
