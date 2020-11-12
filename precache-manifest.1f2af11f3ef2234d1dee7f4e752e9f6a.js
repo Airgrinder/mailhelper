01b9eb7a6c559245dc1edfeb043d081eb6c2da95
Airgrinder <51jework@mail.com>
2020-11-12 16:27:30 +0300

    Updates

---
+++ precache-manifest.1f2af11f3ef2234d1dee7f4e752e9f6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "369c4c0d5e7b3c09c3dbc47b5ffb9170",
+    "url": "/mailhelper/index.html"
+  },
+  {
+    "revision": "c77e61e43516ecc2b10d",
+    "url": "/mailhelper/static/css/main.31bb01a1.chunk.css"
+  },
+  {
+    "revision": "92bc0c48b2c425311876",
+    "url": "/mailhelper/static/js/2.dfb825d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mailhelper/static/js/2.dfb825d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c77e61e43516ecc2b10d",
+    "url": "/mailhelper/static/js/main.eb2bcab5.chunk.js"
+  },
+  {
+    "revision": "21619cb81382a35ae6d1",
+    "url": "/mailhelper/static/js/runtime-main.96d8ea46.js"
+  }
+]);
